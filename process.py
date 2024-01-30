b49f929743e8a7e6d07215f6cef2bb16106851a4
kaixin <156627136+hcgdujcgfjkhv@users.noreply.github.com>
2024-01-30 16:51:05 +0800

    Update process.py

--- process.py
+++ process.py
@@ -335,4 +335,4 @@ def getUserEnergyAward(mobile: str):
                              headers=headers, json={})
     # response.json().get('message') if '无法领取奖励' in response.text else "领取奖励成功"
     logging.info(
-        f'领取耐力 : mobile:{mobile} :  response code : {response.status_code}, response body : {response.text}')
+        f'领取耐力 : mobile: :  response code : {response.status_code}, response body : {response.text}')
